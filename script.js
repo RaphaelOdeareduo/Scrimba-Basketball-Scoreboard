60d2f84c2cc019daf996975d8e85c70fb29c64c4
RaphaelOdeareduo <odeareduor@mymacewan.ca>
2024-09-09 14:23:10 -0600

    Add files

---
+++ script.js
@@ -0,0 +1,16 @@
+let homeScore = 0;
+let awayScore = 0;
+
+let homeScoreEl = document.getElementById("home-score");
+let awayScoreEl = document.getElementById("away-score");
+
+homeScoreEl.textContent = homeScore;
+awayScoreEl.textContent = awayScore;
+
+let addOneHomeBtn = document.getElementById("btnAdd1Home");
+let addTwoHomeBtn = document.getElementById("btnAdd2Home");
+let addThreeHomeBtn = document.getElementById("btnAdd3Home");
+
+let addOneAwayBtn = document.getElementById("btnAdd1Away");
+let addTwoAwayBtn = document.getElementById("btnAdd2Away");
+let addThreeAwayBtn = document.getElementById("btnAdd3Away");
